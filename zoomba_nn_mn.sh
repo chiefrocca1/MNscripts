c1eec70e0d9484c46c19696edc2f45e383fcf045
chiefrocca1 <42627443+chiefrocca1@users.noreply.github.com>
2019-03-03 23:33:19 -0500

    Update zoomba_nn_mn.sh

--- zoomba_nn_mn.sh
+++ zoomba_nn_mn.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
-
+#
 #Setup Variables
 GREEN='\033[0;32m'
 YELLOW='\033[0;93m'
 RED='\033[0;31m'
 NC='\033[0m'
-
+#
 #Checking OS
 if [[ $(lsb_release -d) != *16.04* ]]; then
   echo -e ${RED}"The operating system is not Ubuntu 16.04. You must be running on ubuntu 16.04."${NC}
   exit 1
 fi
-
+#
 echo -e ${YELLOW}"Welcome to the Zoomba Automated Install, During this Process Please Hit Enter or Input What is Asked."${NC}
 echo
 echo -e ${YELLOW}"You Will See alot of code flashing across your screen, don't be alarmed it's supposed to do that. This process can take up to an hour and may appear to be stuck, but I can promise you it's not."${NC}
 echo
 echo -e ${GREEN}"Are you sure you want to install a Zoomba Masternode? type y/n followed by [ENTER]:"${NC}
 read AGREE
-
-
-if [[ $AGREE =~ "y" ]] ; then
-
+#
+if [[ $AGREE =~ "y" ]]; then
+#
 for num in {1..10}
 do
    nn=$(printf "%02d" $num)
 # Use $nn for your purposes
-
+#
 echo -e ${GREEN}"Please Enter Your Masternodes Private Key for node $nn:"${NC}
 read PRIV_KEY_$nn
 #read -p "Enter Your Private Key for Masternode $nn: " PRIV_KEY_$nn
-
-Done
-
+#
+done
+#
 sudo apt-get -y update 
 sudo apt-get -y upgrade
 sudo apt-get -y install software-properties-common 
@@ -43,13 +42,13 @@ sudo apt-get -y install libevent-dev
 sudo apt-get -y install libboost-all-dev 
 sudo apt-get -y install pkg-config  
 sudo add-apt-repository ppa:bitcoin/bitcoin 
-sudo apt-get update 
+sudo apt-get -y update 
 sudo apt-get -y install libdb4.8-dev 
 sudo apt-get -y install libdb4.8++-dev 
 sudo apt-get -y install libminiupnpc-dev libzmq3-dev libevent-pthreads-2.0-5 
 sudo apt-get -y install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev
 sudo apt-get -y install libqrencode-dev bsdmainutils 
-
+#
 echo "Installing Fail2ban and Ufw(Firewall)"
 sudo apt-get -y install fail2ban ufw
 service fail2ban restart
@@ -58,10 +57,9 @@ ufw default allow outgoing
 ufw allow ssh
 ufw allow 5530/tcp
 yes | ufw enable
-
-
-sudo apt install git 
-
+#
+sudo apt -y install git 
+#
 cd /var 
 sudo touch swap.img 
 sudo chmod 600 swap.img 
@@ -70,7 +68,7 @@ sudo mkswap /var/swap.img
 sudo swapon /var/swap.img 
 sudo echo ' /var/swap.img none swap sw 0 0 ' >> /etc/fstab
 cd ~ 
-
+#
 sudo git clone https://github.com/zoombacoin/zoomba 
 sudo chmod -R 755 ~/zoomba 
 cd zoomba 
@@ -78,19 +76,18 @@ sudo ./autogen.sh
 sudo ./configure --disable-tests --disable-gui-tests 
 sudo make 
 sudo make install
-
-for num in {1..10}
-do
+#
+for num in {1..10}; do
    nn=$(printf "%02d" $num)
 # Use $nn for your purposes
 port=$((nn * 2 + 5530))
-
+#
 echo -e ${GREEN}"Please Enter Your Masternodes Private Key for node $nn:"${NC}
 read PRIV_KEY_$nn
-
+#
 echo "Creating n Zoomba system users with no-login access:"
 sudo adduser --system --home /home/zoomba_$nn zoomba_$nn
-
+#
 cd /home/zoomba_$nn
 sudo mkdir /home/zoomba_$nn/.zoomba
 sudo touch /home/zoomba_$nn/.zoomba/zoomba.conf 
@@ -136,24 +133,23 @@ echo "addnode=80.211.184.163" >> /home/zoomba_$nn/.zoomba/zoomba.conf
 echo "addnode=196.52.39.2" >> /home/zoomba_$nn/.zoomba/zoomba.conf
 echo "addnode=173.239.219.13" >> /home/zoomba_$nn/.zoomba/zoomba.conf
 echo "addnode=149.28.236.13" >> /home/zoomba_$nn/.zoomba/zoomba.conf
-
+#
 zoombad -datadir=/home/zoomba_$nn/.zoomba -daemon -reindex
 echo "Syncing Masternode $nn ...";
 done
-
+#
 sleep 10
-
-for num in {1..10}
-do
+#
+for num in {1..10}; do
    nn=$(printf "%02d" $num)
 # Use $nn for your purposes
 sleep 10
 until zoomba-cli -datadir=/home/zoomba_$nn/.zoomba mnsync status | grep -m 1 '"IsBlockchainSynced" : true,'; do sleep 1 ; done > /dev/null 2>&1
 echo -e ${GREEN}"Masternode $nn is fully synced!"${NC}
 done
-
+#
 echo ""
 echo -e ${GREEN}"Congrats! Your Zoomba coin Masternodes are now installed and started. Please wait from 20-30 minutes in order to give the masternode enough time to sync, then start the node from your wallet, Debug console option"${NC}
 echo "The END. You can close now the SSH terminal session";
-
+#
 fi
